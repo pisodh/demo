77683fa2f2658ab45ea50255f1d3392679122993
pisodh <106474223+pisodh@users.noreply.github.com>
2022-05-29 18:17:25 +0530

    Add files via upload

---
+++ BreakDemo.java
@@ -0,0 +1,14 @@
+class BreakDemo
+{
+    public static void main(String[] args)
+    {
+        for(int i=0; i<5; i++)
+        {
+            if(i==3)
+            {
+                break;
+            }
+            System.out.println("i: "+i);
+        }
+    }
+}
